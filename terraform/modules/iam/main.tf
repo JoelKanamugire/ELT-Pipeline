f3def111026cac6202ae07ef4e9d3f3bd8454b5b
Joeldev <kanamujo@gmail.com>
2026-07-06 19:52:35 -0500

    Add Terraform backend (S3 state + DynamoDB lock) access policy for CI/CD user

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -38,11 +38,47 @@ resource "aws_iam_policy" "cwo_lake_access" {
         Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
         Resource = ["${var.bucket_arn}/*"]
       }
+      
     ]
   })
 }
 
+
 resource "aws_iam_user_policy_attachment" "cicd_lake_access" {
   user       = data.aws_iam_user.cicd.user_name
   policy_arn = aws_iam_policy.cwo_lake_access.arn
+}
+
+resource "aws_iam_policy" "terraform_backend_access" {
+  name        = "cwo-${var.env}-terraform-backend-access"
+  description = "Access to Terraform state bucket and lock table for ${var.env}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "StateBucketAccess"
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject"]
+        Resource = ["arn:aws:s3:::datadevp-tfstate/${var.env}/terraform.tfstate"]
+      },
+      {
+        Sid      = "StateBucketList"
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = ["arn:aws:s3:::datadevp-tfstate"]
+      },
+      {
+        Sid      = "LockTableAccess"
+        Effect   = "Allow"
+        Action   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"]
+        Resource = ["arn:aws:dynamodb:us-east-2:625144383094:table/datadevp-tfstate-lock"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "cicd_backend_access" {
+  user       = data.aws_iam_user.cicd.user_name
+  policy_arn = aws_iam_policy.terraform_backend_access.arn
 }
